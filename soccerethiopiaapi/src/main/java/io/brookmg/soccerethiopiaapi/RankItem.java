4860eb92acb1b16a3893247664d780eccdff2efa
brookmezgu <brookmezgebu64@gmail.com>
2018-12-19 23:35:06 +0300

    -- added some dependencies
    + modified the structure class and added some properties

--- soccerethiopiaapi/src/main/java/io/brookmg/soccerethiopiaapi/RankItem.java
+++ soccerethiopiaapi/src/main/java/io/brookmg/soccerethiopiaapi/RankItem.java
@@ -10,12 +10,20 @@ public class RankItem {
     private String teamIcon; //Icon of the team
     private String teamName; //Name of the team
     private int points; //Team point
+    private int playedGames; //The number of games the team played
+    private int wonGames; //The number of games the team has won
+    private int drawGames; //The number of games the team neither won nor lost
+    private int lostGames; //The number of games the team has lost
 
-    public RankItem(int rank, String teamIcon, String teamName, int points) {
+    public RankItem(int rank, String teamIcon, String teamName, int points, int playedGames, int wonGames, int drawGames, int lostGames) {
         this.rank = rank;
         this.teamIcon = teamIcon;
         this.teamName = teamName;
         this.points = points;
+        this.playedGames = playedGames;
+        this.wonGames = wonGames;
+        this.drawGames = drawGames;
+        this.lostGames = lostGames;
     }
 
     public int getRank() {
@@ -49,4 +57,38 @@ public class RankItem {
     public void setPoints(int points) {
         this.points = points;
     }
+
+
+    public int getPlayedGames() {
+        return playedGames;
+    }
+
+    public void setPlayedGames(int playedGames) {
+        this.playedGames = playedGames;
+    }
+
+    public int getWonGames() {
+        return wonGames;
+    }
+
+    public void setWonGames(int wonGames) {
+        this.wonGames = wonGames;
+    }
+
+    public int getDrawGames() {
+        return drawGames;
+    }
+
+    public void setDrawGames(int drawGames) {
+        this.drawGames = drawGames;
+    }
+
+    public int getLostGames() {
+        return lostGames;
+    }
+
+    public void setLostGames(int lostGames) {
+        this.lostGames = lostGames;
+    }
+
 }
---
+++ soccerethiopiaapi/src/main/java/io/brookmg/soccerethiopiaapi/utils/Constants.java
@@ -0,0 +1,11 @@
+package io.brookmg.soccerethiopiaapi.utils;
+
+/**
+ * Created by BrookMG on 12/19/2018 in io.brookmg.soccerethiopiaapi.utils
+ * inside the project SoccerEthiopia .
+ */
+public class Constants {
+
+    public static String CLUB_RANKING_BASE_URL = "http://www.soccerethiopia.net/football/table/2018-19-premier-league-standing";
+
+}
